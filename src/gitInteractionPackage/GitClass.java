df52a49fdd2de052ebcd8f1c1b0e8e92cdf1c130
Suchhanda Senapati <suchhanda046@gmail.com>
2020-07-06 13:37:34 +0530

    This changes is to check/ verify different actions on Github

---
+++ src/gitInteractionPackage/GitClass.java
@@ -0,0 +1,27 @@
+package gitInteractionPackage;
+
+import org.testng.annotations.Test;
+
+public class GitClass {
+	@Test
+	public void test1() {
+		System.out.println("test1 execution");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2 execution");
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3 execution");
+	}
+	@Test
+	public void test4() {
+		System.out.println("test4 execution");
+	}
+	@Test
+	public void test5() {
+		System.out.println("test5 execution");
+	}
+
+}
